56f46c08809962393f6e0141e9e0786129df4882
PDP2600 <jacks_smirking_revenge@hotmail.com>
2019-01-30 16:55:32 -0500

    Initial commit, close to done WIP

---
+++ CS_Driver.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 18:33:52 2019
+Example driver script for ChartScraper.py
+@author: PDP2600
+"""
+import os
+import sqlite3 as sql
+import pandas as pd
+import numpy as np
+os.chdir("D:\\Libraries\\Documents\\Python Scripts\\Billboard_Chart_Scraper")
+
+import ChartScraper as cs
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+
+dir_path = "D:\\Libraries\\Documents\\Python Scripts\\Billboard_Chart_Scraper"
+csv_path = dir_path + "\\ChartScraper_data\\"
+
+##Debugging lines
+test_file_content = open('Hip-Hop_Song_Chart_content_1977-01-03.txt', 'w')
+#test_file_content_2 = open('URLs_Rumors_bb_mail.txt', 'w')
+
+test_chart = cs.requests.get('https://www.billboard.com/charts/r-b-hip-hop-songs/1977-01-03')
+##Remove \n from the content text, it won't serve any purpose and will only get in the way
+test_chart_str = str(test_chart.content).replace('\\n', '')
+
+test_file_content.write(str(test_chart.content))
+#test_file_content_2.write(str(urls_for_email))
+test_file_content.close()
+#test_file_content_2.close()
+str(test_chart.content).index('<div class="chart-number-one__last-week">')
+str(test_chart.content).index('<div class="chart-details ">')
+#test_chart_rest = test_chart_str[test_chart_str.index('data-rank'):test_chart_str.index('</main>')]
+#chart_list = test_chart_rest.split(sep='<div class="chart-list-item  " data-rank=')
+
+
+#####################################
+#hip_hop_albums = bb_get_multiple_charts('r-b-hip-hop-albums', '1977-01-08', '2018-11-18')
+#hip_hop_songs_test = cs.bb_get_multiple_charts('r-b-hip-hop-songs', '1978-11-11', '1978-11-27')
+#hip_hop_albums.to_csv('2018-11-19_Hip_Hop_Albums_from_1977-01-08.csv')
+#hip_hop_songs.to_csv('2018-11-19_Hip_Hop_Songs_from_1977-01-08.csv')
+
+#hip_hop_songs_test = cs.bb_get_multiple_charts('r-b-hip-hop-songs', '1958-10-20', '1978-11-30')
+
+##Number 1 song "RUmors" has no artist (and doesn't seem to have one in other weekly charts not #1)
+##Artist I think is "Timex Social Club"
+##May need to update the rest of chart extractor too
+###Fix with this #hip_hop_songs.artist = np.where(hip_hop_songs['artist'] == '===<Missing_Artist>===', 'Timex Social Club', hip_hop_songs.artist)
+
+########
+hip_hop_albums = cs.bb_get_multiple_charts('r-b-hip-hop-albums', '1965-01-30', '2018-12-31')
+#Entries were missing an artist for one title, determined it was likely Kenny G
+hip_hop_albums.artist = np.where(hip_hop_albums['artist'] == '===<Missing_Artist>===', 'Kenny G', hip_hop_albums.artist)
+hip_hop_albums.to_csv(csv_path + 'All_Hip_Hop_Albums_from_1965-01-30_to_2018-12-31.csv')
+hip_hop_songs_part_1 = cs.bb_get_multiple_charts('r-b-hip-hop-songs', '1958-10-20', '1978-11-11')
+#There's a weird gap in the weekly charts from 1978-11-12 - 1978-11-14
+#Skipping chart for 1978-11-13 as atm it's only displaying the #1
+hip_hop_songs_part_1_B = cs.bb_get_weekly_chart('r-b-hip-hop-songs', '1978-11-11')
+hip_hop_songs_part_1_B = cs._bb_format_columns(hip_hop_songs_part_1_B)
+hip_hop_songs_part_1 = hip_hop_songs_part_1.append(hip_hop_songs_part_1_B)
+hip_hop_songs_part_2 = cs.bb_get_multiple_charts('r-b-hip-hop-songs', '1978-11-18', '2018-12-31')
+hip_hop_songs = hip_hop_songs_part_1.append(hip_hop_songs_part_2)
+#Entries were missing an artist for one title, determined it was likely Timex Social Club
+hip_hop_songs.artist = np.where(hip_hop_songs['artist'] == '===<Missing_Artist>===', 'Timex Social Club', hip_hop_songs.artist)
+hip_hop_songs.to_csv(csv_path + 'All_Hip_Hop_Songs_from_1958-10-20_to_2018-12-31.csv')
+hot_100 = cs.bb_get_multiple_charts('hot-100', '1958-08-04', '1979-12-31')
+hot_100_2 = cs.bb_get_multiple_charts('hot-100', '1980-01-06', '1999-12-31')
+hot_100 = hot_100.append(hot_100_2)
+hot_100_3 = cs.bb_get_multiple_charts('hot-100', '2000-01-07', '2018-12-31')
+hot_100 = hot_100.append(hot_100_3)
+hot_100.to_csv(csv_path + 'All_Hot_100_from_1958-08-04_to_2018-12-31.csv')
+bb_200 = cs.bb_get_multiple_charts('billboard-200', '1963-08-17', '2018-12-31')
+#Missing artist for album Silhouette appears to be by Kenny G
+bb_200.artist = np.where(bb_200['title'] == 'Silhouette', 'Kenny G', bb_200.artist)
+#Missing artist for album Roots of Country Music (1965) appears to be by Various Artsts
+bb_200.artist = np.where(bb_200['title'] == 'Roots Of Country Music (1965)', 
+                         'Various Artists', bb_200.artist)
+bb_200.to_csv(csv_path + 'All_Billboard_200_from_1963-08-17_to_2018-12-31.csv')
+##Resume here
+pop_songs = cs.bb_get_multiple_charts('pop-songs', '1992-10-03', '2018-12-31')
+pop_songs.to_csv(csv_path + 'All_Pop_Songs_from_1992-10-03_to_2018-12-31.csv')
+rock_songs = cs.bb_get_multiple_charts('rock-songs', '2009-06-20', '2018-12-31')
+rock_songs.to_csv(csv_path + 'All_Rock_Songs_from_2009-06-20_to_2018-12-31.csv')
+latin_songs = cs.bb_get_multiple_charts('latin-songs', '1986-09-20', '2018-12-31')
+latin_songs.to_csv(csv_path + 'All_Latin_Songs_from_1986-09-20_to_2018-12-31.csv') 
+dance_elec_songs = cs.bb_get_multiple_charts('dance-electronic-songs', '2013-01-26', '2018-12-31')
+dance_elec_songs.to_csv(csv_path + 'All_Dance_Electronic_Songs_from_2013-01-26_to_2018-12-31.csv')
+youtube_chart = cs.bb_get_multiple_charts('youtube', '2011-08-27', '2018-12-31')
+youtube_chart.to_csv(csv_path + 'All_YouTube_chart_from_2011-08-27_to_2018-12-31.csv')
+japan_hot_100 = cs.bb_get_multiple_charts('japan-hot-100', '2011-04-09', '2018-12-31')
+japan_hot_100.to_csv(csv_path + 'All_Japan_Hot_100_from_2011-04-09_to_2018-12-31.csv')
+country_songs = cs.bb_get_multiple_charts('country-songs', '1958-10-20', '2018-12-31')
+country_songs.to_csv(csv_path + 'All_Country_Songs_from_2011-04-09_to_2018-12-31.csv')
---
+++ ChartScraper.py
@@ -0,0 +1,326 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 15:55:56 2018
+Functions to access Billboard's weekly charts and extracting chart data
+@author: PDP2600
+"""
+import requests
+import pandas as pd
+import datetime
+import numpy as np
+from time import sleep
+
+def bb_get_weekly_chart(chart_route, chart_date):
+    billboard_url = 'https://www.billboard.com/charts/'
+    valid_chart_routes = ['r-b-hip-hop-albums', 'r-b-hip-hop-songs', 'hot-100', 
+                          'billboard-200', 'pop-songs', 'rock-songs', 
+                          'latin-songs', 'dance-electronic-songs', 
+                          'country-songs', 'youtube', 'japan-hot-100']
+    retry_max = 4
+    if chart_route in valid_chart_routes:
+        request_url = billboard_url + chart_route + "/" + chart_date
+        chart_request_data = _attempt_connections(request_url, retry_max)
+        
+        if chart_request_data.status_code == requests.codes.ok:
+            chart_request_str = str(chart_request_data.content).replace('\\n', '')
+            number_one_df = _bb_extract_number_one_data(chart_request_str, chart_date)
+            rest_of_chart_df = _bb_extract_chart_data(chart_request_str, chart_date)
+            chart_appended_df = number_one_df.append(rest_of_chart_df, sort=False)
+            #chart_appended_df = _bb_format_columns(chart_appended_df)        
+            return chart_appended_df
+        else:
+            print("Connection error on {}, {} times attempted."
+                  .format(billboard_url, retry_max + 1))
+            return pd.DataFrame(columns = ['ranking', 'artist', 'title', 
+                                           'last_week_rank', 'peak_position', 
+                                           'weeks_on_chart', 'chart_date'])
+        
+    else:
+        print("{} is not a valid Billboard chart route".format(chart_route))
+        return pd.DataFrame(columns = ['ranking', 'artist', 'title', 
+                                       'last_week_rank', 'peak_position', 
+                                       'weeks_on_chart', 'chart_date'])
+    
+def bb_get_multiple_charts(chart_route, chart_from_date, chart_to_date):
+    chart_data_collection_df = bb_get_weekly_chart(chart_route, chart_from_date)
+    chart_date_index = datetime.datetime.strptime(
+            chart_from_date, "%Y-%m-%d") + datetime.timedelta(days=7)
+    chart_to_datetime  = datetime.datetime.strptime(chart_to_date, "%Y-%m-%d")
+    while chart_date_index <= chart_to_datetime:
+        #print("Chart Date: {}".format(chart_date_index.strftime('%Y-%m-%d')))
+        chart_data_collection_df = chart_data_collection_df.append(
+                bb_get_weekly_chart(chart_route, 
+                                    chart_date_index.strftime('%Y-%m-%d')))
+        chart_date_index = chart_date_index + datetime.timedelta(days=7)
+        sleep(1)
+    print("Finished gathering data from Billboard.com, formatting data...")
+    chart_data_collection_df = _bb_format_columns(chart_data_collection_df)
+    return chart_data_collection_df
+
+def _attempt_connections(conn_url, retries):
+    """Simple and general function I created to retry making a requests.get 
+    connection attempt. When an error status code is returned it's retried the
+    amount of times passed in.
+
+    Parameters:
+        conn_url (str): A string which is url to attempt the get request with.
+        retries (int): The number of times to retry if the connection failed. 
+        At least one connection is attempted, retries are the number of 
+        attempts if the first fails
+        
+    Output:
+        requests "request" object: Contains the content and meta information of 
+        the last connection attempt (first success, or failure of the last retry).
+    """
+    chart_request_result = requests.get(conn_url)
+    success_bool = chart_request_result.status_code == requests.codes.ok
+    retry_num = 1
+    while (success_bool == False) and (retry_num <= retries):
+        print("{} retry number {}".format(conn_url, retry_num))
+        chart_request_result = requests.get(conn_url)
+        success_bool = chart_request_data.status_code == requests.codes.ok
+        retry_num = retry_num + 1
+    return chart_request_result
+
+def _bb_format_columns(chart_df):
+    """Converts escaped characters for the string values, stripping space 
+    characters from "numeric" values (i.e. chart rank, number of weeks on chart), 
+    and replacing any '' artist values with a missing artist token to make it 
+    more explicit and easy to deal with in the results.
+
+    Parameters:
+        chart_df (DataFrame): DF of chart data, with the following columns
+            being acted on: artist, title, ranking, last_week_rank, 
+            peak_position, and weeks_on_chart
+        
+    Output:
+        DataFrame: Contains the DF which was passed in with the stated formatting
+            applied to the columns acted on.
+    """
+    formatting_chart = chart_df
+    formatting_chart.last_week_rank = (formatting_chart.last_week_rank
+                                       .map(lambda lw: (
+                                               str(lw).replace('-', '0')
+                                               .strip())))
+    formatting_chart.title = (formatting_chart.title
+                              .map(lambda t: (
+                                      t.replace('&amp;', '&')
+                                      .replace('&#039;', "'")
+                                      .replace('&quot;', '"')
+                                      .strip())))
+    formatting_chart.artist = (formatting_chart.artist
+                               .map(lambda a: (
+                                       a.replace('&amp;', '&')
+                                       .replace('&#039;', "'")
+                                       .replace('&quot;', '"')
+                                       .strip())))
+    formatting_chart.ranking = (formatting_chart.ranking
+                                .map(lambda r: str(r).strip()))
+    formatting_chart.peak_position = (formatting_chart.peak_position
+                                      .map(lambda p: str(p).strip()))
+    formatting_chart.weeks_on_chart = (formatting_chart.weeks_on_chart
+                                       .map(lambda wc: str(wc).strip()))
+    formatting_chart.artist = (np.where(formatting_chart['artist'] == '',
+                                        '===<Missing_Artist>===',
+                                        formatting_chart.artist))
+    return formatting_chart
+
+def _bb_extract_number_one_data(content_str, chart_date):
+    """Used for extracting the Number 1 ranked entity in the chart (since 
+    there's different mark up for #1s)
+
+    Parameters:
+        content_str (str): A string coverted version of the contents of a 
+            request Billboard Chart page
+        chart_date (str): date used to retrieve the chart, in the format 
+            'YYYY-MM-DD', identifies the chart the entry belongs to    
+    Output:
+        DataFrame: contains chart data extracted and indexed by 
+            '<chart_date>_<chart_rank>' with the column names: ranking, 
+            artist, title, last_week_rank, peak_position, weeks_on_chart, 
+            chart_date
+    """
+    
+    try:
+        start_index = content_str.index('<div class="chart-number-one__info ">')
+    except ValueError:
+        print("Missing opening Number 1 HTML tag for chart requested on date {}"
+              .format(chart_date))
+        return pd.DataFrame(columns = ['ranking', 'artist', 'title', 
+                                       'last_week_rank', 'peak_position', 
+                                       'weeks_on_chart', 'chart_date'])
+    try:
+        end_index = content_str.index('<div class="chart-details ">')
+    except ValueError:
+        print("Missing closing Number 1 HTML tag for chart requested on date {}\n"
+              .format(chart_date))
+        return pd.DataFrame(columns = ['ranking', 'artist', 'title', 
+                                       'last_week_rank', 'peak_position', 
+                                       'weeks_on_chart', 'chart_date'])
+    number_one_data = content_str[start_index:end_index]
+    print("Number One process success {}".format(chart_date))
+    #Portion to extract out the values of interest
+    this_week_position = '1'
+    peak_position = '1'
+    last_week_position = '0'
+    if '<div class="chart-number-one__last-week">' in number_one_data:
+        number_one_data = number_one_data[number_one_data
+                                          .index('<div class="chart-number-one__last-week">'):]
+        last_week_position = number_one_data[
+                len('<div class="chart-number-one__last-week">'):number_one_data
+                .index('</div>')]
+    weeks_on_chart = '1'
+    if '<div class="chart-number-one__weeks-on-chart">' in number_one_data: 
+        number_one_data = number_one_data[
+                number_one_data
+                .index('<div class="chart-number-one__weeks-on-chart">'):]
+        weeks_on_chart = number_one_data[
+                len('<div class="chart-number-one__weeks-on-chart">'):number_one_data
+                .index('</div>')]
+    number_one_data = number_one_data[
+            number_one_data.index('<div class="chart-number-one__title">'):]
+    title = number_one_data[
+            len('<div class="chart-number-one__title">'):number_one_data
+            .index('</div>')]
+    artist = '===<Missing_Artist>==='
+    if '<div class="chart-number-one__artist">' in number_one_data:
+        number_one_data = number_one_data[
+                number_one_data.index('<div class="chart-number-one__artist">'):]
+        number_one_data = number_one_data[
+                len('<div class="chart-number-one__artist">'):] 
+        artist =  number_one_data[:number_one_data.index('</div>')]
+    if '</a>' in artist:
+        artist = number_one_data[number_one_data
+                                 .index('>')+1:number_one_data.index('</a>')]
+    return pd.DataFrame({'ranking':this_week_position, 
+                         'artist':artist, 'title':title, 
+                         'last_week_rank':last_week_position, 
+                         'peak_position':peak_position, 
+                         'weeks_on_chart':weeks_on_chart, 
+                         'chart_date':chart_date
+                         }, index=[str(chart_date)+"_"+this_week_position.strip()])
+
+def _bb_extract_chart_element(chart_str, chart_date):
+    """For extracting data for a single non-number one chart entry
+
+    Parameters: 
+        content_str (str): A string coverted version of the contents of a 
+            request Billboard Chart page
+        chart_date (str): date used to retrieve the chart, in the format 
+            'YYYY-MM-DD', identifies the chart the entry belongs to
+    
+    Output:
+        DataFrame: contains chart data extracted and indexed by 
+            '<chart_date>_<chart_rank>' with the column names: ranking, 
+            artist, title, last_week_rank, peak_position, weeks_on_chart, 
+            chart_date
+    """    
+
+    title = chart_str[
+            chart_str.index('data-title="')+len('data-title="'):chart_str
+            .index('" data-has-content=')]
+    if '<div class="chart-list-item__rank chart-list-item__rank--long">' in chart_str:
+        chart_str = chart_str[
+                chart_str
+                .index('<div class="chart-list-item__rank chart-list-item__rank--long">'):]
+        this_week_position = chart_str[
+                len('<div class="chart-list-item__rank chart-list-item__rank--long">'):chart_str
+                .index('</div>')]    
+    else:
+        chart_str = chart_str[chart_str.index('<div class="chart-list-item__rank ">'):]
+        this_week_position = chart_str[
+                len('<div class="chart-list-item__rank ">'):chart_str
+                .index('</div>')]
+    
+    chart_str = chart_str[chart_str.index('<div class="chart-list-item__title">'):]
+    #title = chart_str[len('<div class="chart-list-item__title">'):chart_str.index('</div>')]
+    #if '<span class="chart-list-item__title-text">' in title:
+    #    title = chart_str[chart_str.index('>')+1:chart_str.index('</span>')]
+    artist = '===<Missing_Artist>==='
+    if '<div class="chart-list-item__artist">' in chart_str:
+        chart_str = chart_str[chart_str
+                              .index('<div class="chart-list-item__artist">'):]
+        chart_str = chart_str[len('<div class="chart-list-item__artist">'):]
+        artist = chart_str[:chart_str.index('</div>')]
+    if '</a>' in artist:
+        artist = chart_str[chart_str.index('>')+1:chart_str.index('</a>')]
+    last_week_position = '0'
+    if '<div class="chart-list-item__last-week">' in chart_str:
+        chart_str = chart_str[chart_str
+                              .index('<div class="chart-list-item__last-week">'):]
+        last_week_position = chart_str[
+                len('<div class="chart-list-item__last-week">'):chart_str
+                .index('</div>')]
+    peak_position = this_week_position
+    if '<div class="chart-list-item__weeks-at-one">' in chart_str:
+        chart_str = chart_str[
+                chart_str.index('<div class="chart-list-item__weeks-at-one">'):]
+        peak_position = chart_str[
+                len('<div class="chart-list-item__weeks-at-one">'):chart_str
+                .index('</div>')]
+    weeks_on_chart = '1'
+    if '<div class="chart-list-item__weeks-on-chart">' in chart_str:
+        chart_str = chart_str[
+                chart_str.index('<div class="chart-list-item__weeks-on-chart">'):]
+        weeks_on_chart = chart_str[
+                len('<div class="chart-list-item__weeks-on-chart">'):chart_str
+                .index('</div>')]
+    return pd.DataFrame({'ranking':this_week_position, 
+                         'artist':artist, 'title':title, 
+                         'last_week_rank':last_week_position, 
+                         'peak_position':peak_position, 
+                         'weeks_on_chart':weeks_on_chart, 
+                         'chart_date':chart_date
+                         }, index=[str(chart_date)+"_"+this_week_position.strip()])
+
+def _bb_extract_chart_data(content_str, chart_date):
+    """Takes chart data, removes the number one entry data, slices the chart 
+    data up into a list per chart entry & then calls the chart data extraction 
+    function on each chart entry chunk.
+
+    Parameters: 
+        content_str (str): A string coverted version of the contents of a 
+            request Billboard Chart page
+        chart_date (str): date used to retrieve the chart, in the format 
+            'YYYY-MM-DD', identifies the chart the entry belongs to
+    Output: 
+        DataFrame: contains chart data extracted and indexed by 
+            '<chart_date>_<chart_rank>' with the column names: ranking, 
+            artist, title, last_week_rank, peak_position, weeks_on_chart, 
+            chart_date    
+    """
+    number_1_data_end = content_str.index('<div class="chart-details ">')
+    try:
+        end_index = content_str.index('</main>') + len('</main>')
+    except ValueError:
+        print("Missing closing Chart HTML tag for chart requested on date {}\n"
+              .format(chart_date))
+        return pd.DataFrame(columns = ['ranking', 'artist', 'title', 
+                                       'last_week_rank', 'peak_position', 
+                                       'weeks_on_chart', 'chart_date'])
+    lower_chart_data = content_str[number_1_data_end:end_index]
+    try:
+        start_index = lower_chart_data.index('data-rank')
+    except ValueError:
+        print("Missing opening Chart HTML tag for chart requested on date {}\n"
+              .format(chart_date))
+        return pd.DataFrame(columns = ['ranking', 'artist', 'title', 
+                                       'last_week_rank', 'peak_position', 
+                                       'weeks_on_chart', 'chart_date'])
+    end_index = lower_chart_data.index('</main>')
+    
+    lower_chart_data = lower_chart_data[start_index:end_index]
+    lower_chart_list = lower_chart_data.split(
+            sep='<div class="chart-list-item  " data-rank=')
+    chart_parsed_data = pd.DataFrame(columns = ['ranking', 'artist', 'title', 
+                                                'last_week_rank', 
+                                                'peak_position', 
+                                                'weeks_on_chart', 'chart_date'])
+    #count = 2
+    for chart_data in lower_chart_list:
+        #print("{} rank\n".format(count))
+        #print("{}".format(chart_data))
+        chart_entry_data = _bb_extract_chart_element(chart_data, chart_date)
+        chart_parsed_data = chart_parsed_data.append(chart_entry_data, sort=False)
+        #count = count + 1        
+    return chart_parsed_data

188e219396572a2d49274c921066d10846c0590d
PDP2600 <jacks_smirking_revenge@hotmail.com>
2019-02-05 15:03:20 -0500

    Completed README.md 1st draft and cleaned up some fo the scripts

--- CS_Driver.py
+++ CS_Driver.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jan  9 18:33:52 2019
-Example driver script for ChartScraper.py
+Example driver script for chartscraper.py
 @author: PDP2600
 """
 import pandas as pd
 import numpy as np
-#Setup a symlink to the site-packages folder, to the drive I do work on and 
-#created a folder called local_modules to store my personal/local modules
-from local_modules import ChartScraper as cs
+#Syntax specific to how/where I store reusable scripts/local modules
+from local_modules import chartscraper as cs
 
-#import os
-#import sys
-#sys.path.append('D:\\Libraries\\Documents\\local_python_modules')
-#os.chdir("D:\\Libraries\\Documents\\Python Scripts\\Billboard_Chart_Scraper")
-#import ChartScraper as cs
+"""#To set the working directory to the location of chartscraper.py before importing
+import os
+os.chdir("D:\\Libraries\\Documents\\Python Scripts\\Billboard_Chart_Scraper")
+import chartscraper as cs
+"""
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -22,45 +21,14 @@ pd.set_option('display.max_columns', 500)
 dir_path = "D:\\Libraries\\Documents\\Python Scripts\\Billboard_Chart_Scraper"
 csv_path = dir_path + "\\ChartScraper_data\\"
 
-##Debugging lines
-test_file_content = open('Hip-Hop_Song_Chart_content_1977-01-03.txt', 'w')
-#test_file_content_2 = open('URLs_Rumors_bb_mail.txt', 'w')
 
-test_chart = cs.requests.get('https://www.billboard.com/charts/r-b-hip-hop-songs/1977-01-03')
-##Remove \n from the content text, it won't serve any purpose and will only get in the way
-test_chart_str = str(test_chart.content).replace('\\n', '')
-
-test_file_content.write(str(test_chart.content))
-#test_file_content_2.write(str(urls_for_email))
-test_file_content.close()
-#test_file_content_2.close()
-str(test_chart.content).index('<div class="chart-number-one__last-week">')
-str(test_chart.content).index('<div class="chart-details ">')
-#test_chart_rest = test_chart_str[test_chart_str.index('data-rank'):test_chart_str.index('</main>')]
-#chart_list = test_chart_rest.split(sep='<div class="chart-list-item  " data-rank=')
-
-
-#####################################
-#hip_hop_albums = bb_get_multiple_charts('r-b-hip-hop-albums', '1977-01-08', '2018-11-18')
-#hip_hop_songs_test = cs.bb_get_multiple_charts('r-b-hip-hop-songs', '1978-11-11', '1978-11-27')
-#hip_hop_albums.to_csv('2018-11-19_Hip_Hop_Albums_from_1977-01-08.csv')
-#hip_hop_songs.to_csv('2018-11-19_Hip_Hop_Songs_from_1977-01-08.csv')
-
-#hip_hop_songs_test = cs.bb_get_multiple_charts('r-b-hip-hop-songs', '1958-10-20', '1978-11-30')
-
-##Number 1 song "RUmors" has no artist (and doesn't seem to have one in other weekly charts not #1)
-##Artist I think is "Timex Social Club"
-##May need to update the rest of chart extractor too
-###Fix with this #hip_hop_songs.artist = np.where(hip_hop_songs['artist'] == '===<Missing_Artist>===', 'Timex Social Club', hip_hop_songs.artist)
-
-########
 hip_hop_albums = cs.bb_get_multiple_charts('r-b-hip-hop-albums', '1965-01-30', '2018-12-31')
 #Entries were missing an artist for one title, determined it was likely Kenny G
 hip_hop_albums.artist = np.where(hip_hop_albums['artist'] == '===<Missing_Artist>===', 'Kenny G', hip_hop_albums.artist)
 hip_hop_albums.to_csv(csv_path + 'All_Hip_Hop_Albums_from_1965-01-30_to_2018-12-31.csv')
 hip_hop_songs_part_1 = cs.bb_get_multiple_charts('r-b-hip-hop-songs', '1958-10-20', '1978-11-11')
 #There's a weird gap in the weekly charts from 1978-11-12 - 1978-11-14
-#Skipping chart for 1978-11-13 as atm it's only displaying the #1
+#Skipping chart for 1978-11-13 as at the time it was only displaying the #1
 hip_hop_songs_part_1_B = cs.bb_get_weekly_chart('r-b-hip-hop-songs', '1978-11-11')
 hip_hop_songs_part_1_B = cs._bb_format_columns(hip_hop_songs_part_1_B)
 hip_hop_songs_part_1 = hip_hop_songs_part_1.append(hip_hop_songs_part_1_B)
@@ -82,7 +50,6 @@ bb_200.artist = np.where(bb_200['title'] == 'Silhouette', 'Kenny G', bb_200.arti
 bb_200.artist = np.where(bb_200['title'] == 'Roots Of Country Music (1965)', 
                          'Various Artists', bb_200.artist)
 bb_200.to_csv(csv_path + 'All_Billboard_200_from_1963-08-17_to_2018-12-31.csv')
-##Resume here
 pop_songs = cs.bb_get_multiple_charts('pop-songs', '1992-10-03', '2018-12-31')
 pop_songs.to_csv(csv_path + 'All_Pop_Songs_from_1992-10-03_to_2018-12-31.csv')
 rock_songs = cs.bb_get_multiple_charts('rock-songs', '2009-06-20', '2018-12-31')
@@ -97,3 +64,23 @@ japan_hot_100 = cs.bb_get_multiple_charts('japan-hot-100', '2011-04-09', '2018-1
 japan_hot_100.to_csv(csv_path + 'All_Japan_Hot_100_from_2011-04-09_to_2018-12-31.csv')
 country_songs = cs.bb_get_multiple_charts('country-songs', '1958-10-20', '2018-12-31')
 country_songs.to_csv(csv_path + 'All_Country_Songs_from_2011-04-09_to_2018-12-31.csv')
+
+#################################################################
+"""
+##Debugging lines
+test_file_content = open('Hip-Hop_Song_Chart_content_1977-01-03.txt', 'w')
+#test_file_content_2 = open('URLs_Rumors_bb_mail.txt', 'w')
+
+test_chart = cs.requests.get('https://www.billboard.com/charts/r-b-hip-hop-songs/1977-01-03')
+##Remove \n from the content text, it won't serve any purpose and will only get in the way
+test_chart_str = str(test_chart.content).replace('\\n', '')
+
+test_file_content.write(str(test_chart.content))
+#test_file_content_2.write(str(urls_for_email))
+test_file_content.close()
+#test_file_content_2.close()
+str(test_chart.content).index('<div class="chart-number-one__last-week">')
+str(test_chart.content).index('<div class="chart-details ">')
+#test_chart_rest = test_chart_str[test_chart_str.index('data-rank'):test_chart_str.index('</main>')]
+#chart_list = test_chart_rest.split(sep='<div class="chart-list-item  " data-rank=')
+"""
--- README.md
+++ README.md
@@ -1,15 +1,42 @@
-#### 
+Script for scraping weekly Billboard charts from their website. In the future I intend to write functions extending the local module to gather other, more niche/underground charts. Datasets provided from the beginning of the chart to the year end of 2018, for all the charts the local module has been tested with.
 
-Need to add the markdown readme content here. It'll mostly be about what the local module does and the quirks about Billboards chart site which I think would be useful to share.
+Scripts & dataset available to use under GNU General Public License, see LICENSE.txt for full details (basically all uses & modifications allowed as long as the original is cited, and no usage in closed source applications).
 
-#Might need to add this information about appending a new Python related path to look for modules:
->>> import sys 
->>> sys.path.append('/ufs/guido/lib/python')
+***
 
-Solution to create a symlink to the site packages folder which is in the python path
-mklink /D link target
-#Linux
-ln -s /path/to/my/package /path/to/anaconda/env/myenv/lib/python2.7/site-packges/
-#WIndows
-mklink /D D:\Libraries\Documents\local_python_modules C:\Users\jacks\Anaconda3\lib\site-packages
+#### Files
+- ***README.md*** - Current file.
+- ***LICENSE.txt*** - Full GNU General Public License.
+- ***chartscraper.py*** - Billboard chart web scraping local script/module.
+- ***CS\_Driver.py*** - Script used to create the dataset & example of how to use _chartscraper_.
+- ***\ChartScraper\_data\*** - Folder with CSVs containing data from the beginning of each chart to the last one in 2018, from the following Billboard charts:
+	- [Hot R&B/Hip\-hop Albums](https://www.billboard.com/charts/r-b-hip-hop-albums)
+	- [Hot R&B/Hip\-hop Songs](https://www.billboard.com/charts/r-b-hip-hop-songs)
+	- [Hot 100](https://www.billboard.com/charts/hot-100)
+	- [Billboard 200](https://www.billboard.com/charts/billboard-200)
+	- [Pop Songs](https://www.billboard.com/charts/pop-songs)
+	- [Hot Rock Songs](https://www.billboard.com/charts/rock-songs)
+	- [Hot Latin Songs](https://www.billboard.com/charts/latin-songs)
+	- [Hot Dance/Electronic Songs](https://www.billboard.com/charts/dance-electronic-songs)
+	- [YouTube](https://www.billboard.com/charts/youtube)
+	- [Japan Hot 100](https://www.billboard.com/charts/japan-hot-100)
+	- [Hot Country Songs](https://www.billboard.com/charts/country-songs)
 
+***
+	
+#### Usage
+The following non-stock modules are utilized (they're all included in the Anaconda distro of Python): 
+* [requests](http://docs.python-requests.org/en/master/)
+* [pandas](https://pandas.pydata.org/)
+* [numpy](http://www.numpy.org/)
+
+Local module file _chartscraper.py_ must be in a location which is a part of the PYHTONPATH or the working directory needs to be changed to its location when it's imported (commented example found in *CS\_Driver.py*)
+
+Main functions are *bb\_get\_weekly\_chart* & *bb\_get\_multiple\_charts*, all functions including internal ones, have help documentation. Included script *CS\_Driver.py* contains an example of how they're used. Worth noting, I have hard coded a 1 second delay between multiple requests to avoid flooding.
+
+##### Notes About Billboard's Site/Data
+- URL structure for weekly charts looks like `https://www.billboard.com/charts/r-b-hip-hop-songs/2019-02-02`
+	- You don't need to pass the specific date used to denote the week of the chart, Billboard's backend will take any date a part of the week the chart is for.
+- There is some potential weirdness with the data which I only encountered once in the R&B/Hip\-Hop songs chart, where between _1978-11-13_ to _1978-11-18_ it had a chart with only the number 1 song. When this case occurs, the data is added and there's a text output notification which should be a prompt to look into the occurence, and make sure no legit charts were missed
+- In some cases there are missing artist names. When an artist value is None/null, the string token _"===<Missing_Artist>===" is substituted to make it more explicit.
+	- I believe I took care of all the missing artists in the dataset, replacing them with the correct values after doing some research myself. I reported the first one I found in 2018 to the email address in the Billboard site FAQ which was for data corrections, but 4 months later the data was not corrected, so any extra effort of benevolence might be wasted.
